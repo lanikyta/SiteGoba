17d8396fc99ab407eedc46c16d4e9f3e8e6b794d
Aylen Cruz <lacuriosa_jael_29@hotmail.com>
2024-02-09 14:21:00 -0300

    building

--- src/components/layout/ShopLayout.jsx
+++ src/components/layout/ShopLayout.jsx
@@ -2,7 +2,7 @@ import { Box, useColorModeValue } from '@chakra-ui/react'
 import { Footer } from './Footer'
 import { Navbar } from './Navbar'
 const ShopLayout = ({ children }) => {
-  const color = useColorModeValue('cyan.200', 'blackAlpha.700')
+  const color = useColorModeValue('blackAlpha.700', 'cyan.200')
   return (
     <Box minH="100vh" maxW="100vw" bg="#ffffff" color={color}>
       <Navbar />
